491a826aad120f373b59c246e281f57511dd470d
jayharris14 <70274419+jayharris14@users.noreply.github.com>
2020-09-29 18:29:30 -0400

    Add files via upload

---
+++ nameandpic.js
@@ -0,0 +1,28 @@
+
+var getFirstName= function(employees){
+var  firstname= employees.firstName
+var lastname=employees.lastName
+return firstname+" "+ lastname
+}
+var  firstnames=employees.map(getFirstName)
+
+var getLastName= function(employees){
+return employees.lastName
+}
+var  lastnames=employees.map(getLastName)
+
+d3.select("body")
+  .selectAll("h2")
+  .data(employees)
+  .enter()
+  .append("h2")
+  .text(getFirstName)
+
+
+  d3.select("body")
+    .selectAll("img")
+    .data(employees)
+    .enter()
+    .append("img")
+    .attr("src", function(employees){
+    return employees.photo})
